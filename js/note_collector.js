ad1651b6e47d09837940db86d9791358bba0c266
anupam <anupamdas0376@gmail.com>
2020-07-15 16:14:41 +0600

    2 files added

---
+++ js/note_collector.js
@@ -0,0 +1,85 @@
+let showNote=document.getElementById("showNote");
+showNote.addEventListener("click",function()
+{
+shownotes();
+});
+let addbtn = document.getElementById("addButton");
+addbtn.addEventListener("click", function (e) {
+    let addtext = document.getElementById("textArea");
+    let valueTest=addtext.value;
+    console.log(valueTest);
+    
+    if(valueTest.match(/\S/)){
+    
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addtext.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addtext.value = " ";
+    
+    shownotes();
+    }
+
+
+})
+//function to show item
+
+function shownotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    let html = " ";
+    notesObj.forEach(function (element,index) {
+
+        html += `
+        <div class="notecard my-2 mx-2" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">NOTES ${index+1}</h5>
+            <p class="card-text">${element}</p>
+            <button id ="${index}" onclick="deleteNote(this.id)"class="btn btn-primary">Delete Note</button>
+        </div>
+        </div>
+             `;
+
+    });
+    let notesElm=document.getElementById("notes");
+    if(notes.length!=0)
+    {
+        notesElm.innerHTML=html;
+    }
+    else 
+    {
+    notesElm.innerHTML=`Sorry , NO Note Is Available`;
+    }
+}
+
+
+
+//fucntion to delete node
+
+function deleteNote(index)
+{
+   let notes=localStorage.getItem("notes");
+   if(notes==null)
+   {
+       notesObj=[]
+   }
+   else
+   {
+       notesObj=JSON.parse(notes);
+   }
+
+
+   notesObj.splice(index,1);
+   localStorage.setItem("notes",JSON.stringify(notesObj));
+   shownotes();
+
+
+}
